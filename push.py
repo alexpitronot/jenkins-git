5af45e133b76be0811ca771e16f80fe6fc82ef59
Alex Gorbachov <alexpitronot@gmail.com>
2021-08-30 19:12:57 +0300

    Create push.py

---
+++ push.py
@@ -0,0 +1,16 @@
+import os
+if not os.path.exists("data.txt"):  
+    file1 = open("data.txt","a") # use "a" to append data to existing file
+
+    fName = "Alex"
+    lName = "Gorbachov"
+    age = 43
+    id_n = "31882222"
+
+    Hello_Message = 'Hello {} {}, {} years old, with ID number: {}.'
+
+
+    file1.write(Hello_Message.format(fName, lName, age, id_n))
+    file1.close()
+else:
+    exit(1)
